edf4a20cf4ba53a41e78dedcf8b447450afcde73
Andrey Setyaev <asetyaev@mirantis.com>
2016-03-25 15:45:16 +0000

    Special script to manipulate with VMware nodes and snapshots.

---
+++ vmware.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[ -z "$1" ] && { echo "Usage:"; echo "command [snapshot_name]"; echo "Available commands: listRegisteredVM, revertToSnapshot, listSnapshots"; exit 1; }
+
+login='vmware'
+pass='VMware01'
+ext_nodes="esxi1 esxi2 esxi3 vcenter trusty"
+
+for i in $ext_nodes
+do
+  vmrun -T ws-shared -h https://localhost:443/sdk -u $login -p $pass "$1" "[standard] $i/$i.vmx" "$2"
+done
